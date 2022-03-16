cb6ad76b160f3206aa568c5ed359a57e812617e8
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-03-16 16:56:43 +0000

    Relacionamentos um para muitos e muitos para muitos

--- ifbookbackend/blueprints/exemplo/exemplo.py
+++ ifbookbackend/blueprints/exemplo/exemplo.py
@@ -1,28 +1,78 @@
 from flask import Blueprint
 
 from ifbookbackend.ext.database import db
-from ifbookbackend.models import User
+from ifbookbackend.models import User, Curso
 
 bp = Blueprint("exemplo", __name__, url_prefix="/exemplo", template_folder="templates")
 
 
-@bp.route("/")
-def root():
-    busca = "eli"
-    usuario = User.query.filter_by(username=f"%{busca}%").first()
-    return f"nome: {usuario.username} <br>Senha: {usuario.senha}"
+@bp.route("/<busca>")
+def root(busca):
+    usuario = User.query.filter_by(username=busca).first()
+    cursos = ""
+    for curso in usuario.cursos:
+        print(curso)
+        cursos += f"{curso.sigla}, "
+    return f"nome: {usuario.username} <br>Cursos: {cursos}"
 
 
-@bp.route("/<nome>/<senha>")
-def salva(nome, senha):
+@bp.route("/alunosdocurso/<s>")
+def alunosdocurso(s):
+    curso = Curso.query.filter_by(sigla=s).first()
+
+    if curso:
+        resposta = ""
+        for usuario in curso.users:
+            resposta += f"{usuario.username}, "
+        return resposta
+    else:
+        return "Curso não existe", 404
+
+
+@bp.route("/addcurso/<nome>/<sigla>")
+def salvaCurso(nome, sigla):
+    novo = Curso()
+    novo.nome = nome
+    novo.sigla = sigla
+
+    db.session.add(novo)
+    db.session.commit()
+
+    return "Curso cadastrado"
+
+
+@bp.route("/adduser/<nome>/<senha>/<descricao>/<int:curso_id>")
+def salva(nome, senha, descricao, curso_id):
     novo = User()
     novo.username = nome
     novo.senha = senha
+    novo.descricao = descricao
+
+    curso_da_pessoa = Curso.query.get(curso_id)
+
+    if curso_da_pessoa:
+        novo.cursos.append(curso_da_pessoa)
+    else:
+        return "Da próxima vez vá se cadastrar sem curso na casa da sua avó.", 400
 
     db.session.add(novo)
     db.session.commit()
 
     return "deu certo"
+
+
+@bp.route("/novocurso/<int:ida>/<int:idb>")
+def amigos(ida, idb):
+    pessoa = User.query.get(ida)
+
+    novocurso = Curso.query.get(idb)
+
+    pessoa.cursos.append(novocurso)
+
+    db.session.add(pessoa)
+    db.session.commit()
+
+    return "Você tem um novo curso."
 
 
 def init_app(app):
--- ifbookbackend/models.py
+++ ifbookbackend/models.py
@@ -1,8 +1,25 @@
 from ifbookbackend.ext.database import db
 
 
+class Curso(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(30), unique=True, nullable=False)
+    sigla = db.Column(db.String(4), unique=True)
+
+
+cursos_alunos = db.Table(
+    "cursos_alunos",
+    db.Column("aluno", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("curso", db.Integer, db.ForeignKey("curso.id"), primary_key=True),
+)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     senha = db.Column(db.String(100), nullable=False)
     descricao = db.Column(db.String(1000), default="")
+
+    cursos = db.relationship(
+        "Curso", secondary=cursos_alunos, backref=db.backref("users")
+    )
